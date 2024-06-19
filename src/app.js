3b5cb6f86aba5a509cdbbab54d56f20a1f22d49e
bk-007 <bry2163447@maricopa.edu>
2024-06-19 12:13:55 -0700

    update js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import lottie from 'lottie-web';
 
 let animation = lottie.loadAnimation({
-    container: document.getElementById('hero-animation'), // the dom element that will contain the animation
+    container: document.getElementById('hero-animation'), 
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: 'src/az-backdrop.json' // the path to the animation json
+    path: 'az-backdrop.json'
   });
   
